3933b7815b97ec53fbb870b9584d3ac395effc29
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-10-15 18:22:17 +0530

    ADDED:creating the tree using the size method

--- src/main/java/com/bridgelabz/BinarySearchTree.java
+++ src/main/java/com/bridgelabz/BinarySearchTree.java
@@ -50,7 +50,18 @@ public class BinarySearchTree<k extends Comparable<k>> {
         BinarySearchTree<Integer> element = new BinarySearchTree<>();
         element.add(56);
         element.add(30);
-        element.add(70);
+        element.add(22);
+        element.add(40);
+        element.add(60);
+        element.add(95);
+        element.add(11);
+        element.add(65);
+        element.add(3);
+        element.add(16);
+        element.add(63);
+        element.add(67);
         element.printNodes();
+        int size = element.getSize();
+        System.out.println("Size of tree "+" " +size);
     }
 }
